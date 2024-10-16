412beb639447325309ebaa225a11fe17c5b8b5d3
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-16 16:53:12 +0530

    Area of Rectangle

---
+++ JAVA_Class_08/C8_Prog_001.java
@@ -0,0 +1,11 @@
+// Area of given rectangle
+
+class RectangleArea
+{
+    public static void main(String[]args)
+    {
+        int length = 50, breadth = 15; 
+        int area = length * breadth;
+        System.out.print ("Area of given rectangle= " +area);
+    }
+}

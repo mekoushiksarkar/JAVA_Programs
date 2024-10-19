2fceb9bf497a809df9915c4a94e0060f62aebf54
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-19 11:00:51 +0530

    Average of Numbers

---
+++ JAVA_Class_08/C8_Prog_002.java
@@ -0,0 +1,11 @@
+// Average of 5,4 and 90
+
+class Average
+{
+    public static void main(String[] args) 
+    {
+        int x=5, y=4, z=90;
+        double avg= (x+y+z)/3.0;
+        System.out.print("Average of 5,4 and 90 is: " +avg);
+    }
+}

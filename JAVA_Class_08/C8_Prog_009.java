5dba6ad3c5a784e8694cbe0263ecb2af3ceb9533
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-28 11:11:37 +0530

    PRINT THE FIRST 10 NATURAL NUMBERS

---
+++ JAVA_Class_08/C8_Prog_009.java
@@ -0,0 +1,13 @@
+//PROGRAM TO PRINT THE FIRST 10 NATURAL NUMBERS
+
+class Loops
+{
+	public static void main(String[]args)
+	{
+		int i;
+		for(i=1; i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}

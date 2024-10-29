587b2475a33828d6ad00e8c6946bd723dca666ac
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-29 09:27:22 +0530

    Sum of Natural Number

---
+++ JAVA_Class_08/C8_Prog_010.java
@@ -0,0 +1,14 @@
+// PROGRAM TO FIND THE SUM OF THE FIRST 5 NATURAL NUMBERS
+
+class Loops
+{
+    public static void main(String[]args)
+    {
+        int c, s=0;
+        for(c=1; c<=5; c++)
+        {
+            s=s+c;
+        }
+        System.out.println("The sum is="+s);
+    }
+}

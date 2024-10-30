8258a8fa8fed8b0e090582f527e31c657865879e
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-30 19:02:06 +0530

    Multiple Updation

---
+++ JAVA_Class_08/C8_Prog_011.java
@@ -0,0 +1,13 @@
+// MULTIPLE INITIALISATION AND MULTIPLE UPDATION
+
+class Loops
+{
+	public static void main(String[]args)
+	{
+		int i,j;
+		for(i=1,j=10;i<=10;j--,i++)
+		{
+			System.out.println("i="+i+" "+"j="+j);
+		}
+	}
+}

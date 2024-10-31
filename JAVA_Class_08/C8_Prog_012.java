f99cf5946d69628aab7821f7ab45da2db0bb1210
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-10-31 16:07:16 +0530

    Print first 10 Odd Numbers

---
+++ JAVA_Class_08/C8_Prog_012.java
@@ -0,0 +1,13 @@
+// FIRST 10 ODD NUMBERS
+
+class OddNumbers
+{
+    public static void main(String[]args)
+    {
+        int i;
+        for(i=1;i<=19;i+=2)
+        {
+            System.out.println(i+"");
+        }
+    }
+}

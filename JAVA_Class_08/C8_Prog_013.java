2ab575bbefa12862e2c6759e03a0de118cb0e610
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-11-01 11:43:31 +0530

    Multiple of 5

---
+++ JAVA_Class_08/C8_Prog_013.java
@@ -0,0 +1,13 @@
+// MULTIPLES OF 5
+
+class MultiplesOf5
+{
+    public static void main(String[]args)
+    {
+        int i;
+        for(i=5;i<=50;i+=5)
+        {
+            System.out.println(i+"");
+        }
+    }
+}

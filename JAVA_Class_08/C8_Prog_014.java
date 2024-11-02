e5c3b4ebf3ecd4104c81c4502303dc79c3371659
Koushik Sarkar <koushik.sarkar535@outlook.com>
2024-11-02 16:42:20 +0530

    Multiples of 7

---
+++ JAVA_Class_08/C8_Prog_014.java
@@ -0,0 +1,14 @@
+// MULTIPLES OF 7
+
+class MultiplesOf7
+{
+    public static void main(String[]args)
+    {
+        int i;
+        for(i=10;i<=99;i++)
+        {
+            if(i%7==0)
+               System.out.println(i+"");
+        }
+    }
+}

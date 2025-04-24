95aad13b78b1ea6094c1830a42b3721cddfb8675
Koushik Sarkar <koushik.sarkar535@outlook.com>
2025-04-24 12:25:30 +0530

    Sum Series: 1/2 + 1/4 + 1/6 + ...

---
+++ JAVA_Class_08/C8_Prog_041.java
@@ -0,0 +1,21 @@
+// Sum Series: 1/2 + 1/4 + 1/6 + .....
+
+import java.util.*;
+class Main 
+{
+    public static void main(String[] args) 
+	{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the range:");
+        double n=sc.nextInt();
+        double i,s=0;
+        for(i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                s=s+1/i;
+            }
+        }
+        System.out.println("The Sum of the series is: "+s);
+    }
+}

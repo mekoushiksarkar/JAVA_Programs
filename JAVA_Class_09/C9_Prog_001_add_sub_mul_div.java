3160642e896db44750d683928499997051044c71
Koushik Sarkar <koushik.sarkar535@outlook.com>
2025-06-15 12:37:07 +0530

    Add, substract, multiply, division

---
+++ JAVA_Class_09/C9_Prog_001_add_sub_mul_div.java
@@ -0,0 +1,18 @@
+// WAP to take two numbers and make them add, substract, multiply, division
+
+public class add_sub_mul_div {
+
+	public static void main(String[]args) {
+		int a, b, add = 0, sub = 0, mul = 0, div = 0;
+		a = 10;
+		b = 5;
+		add = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a / b;
+		System.out.println("ADDITION IS: " + add);
+		System.out.println("SUBSTRACTION IS: " + sub);
+		System.out.println("MULTIPLICATION IS: " + mul);
+		System.out.println("DIVISION IS: " + div);
+	}
+}

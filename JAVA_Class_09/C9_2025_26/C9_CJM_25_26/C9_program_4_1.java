e0910f39cd719fa040d35699eefc73e7e02e8e83
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-01-28 11:41:09 +0530

    Sum of Two Numbers

---
+++ JAVA_Class_09/C9_2025_26/C9_CJM_25_26/C9_program_4_1.java
@@ -0,0 +1,25 @@
+//To find the sum of two numbers
+
+import java.util.*;
+class Sum
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        double a=sc.nextDouble();
+        System.out.println("Enter the second number:");
+        double b=sc.nextDouble();
+        double c=a+b;
+        System.out.println("The sum of the two numbers="+c);
+        
+    }
+}  
+
+
+output:-
+Enter the first number:
+956.54853
+Enter the second number:
+6.87557423
+The sum of the two numbers=963.42410423

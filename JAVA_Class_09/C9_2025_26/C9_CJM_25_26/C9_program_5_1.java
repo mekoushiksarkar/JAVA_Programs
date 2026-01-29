13c3873437a6a6d2aac3ca197fb13bb091e128e0
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-01-29 16:42:56 +0530

    Calculate Velocity

---
+++ JAVA_Class_09/C9_2025_26/C9_CJM_25_26/C9_program_5_1.java
@@ -0,0 +1,20 @@
+//A program to calculate the final velocity
+
+import java.util.*;
+public class Velocity
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of p:");
+        double p=sc.nextDouble();
+        double d=++p+5;
+        System.out.println("The value is:"+d);
+    }
+}
+
+Output
+
+Enter the value of p:
+13
+The value is:19.0

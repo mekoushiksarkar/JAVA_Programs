66d34da8ab7178213f64db2d7f8f19d8fcb3dd93
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-02-03 17:17:07 +0530

    MULTIPLES OF 5

---
+++ JAVA_Class_09/C9_2025_26/C9_CJM_25_26/C9_program_6_13.java
@@ -0,0 +1,26 @@
+//MULTIPLES OF 5
+
+class MultiplesOf5 {
+
+  public static void main(String[] args) {
+    int i;
+    for (i = 5; i <= 50; i += 5) {
+      System.out.println(i + "");
+    }
+  }
+}
+
+// OUTPUT
+
+// 5
+// 10
+// 15
+// 20
+// 25
+// 30
+// 35
+// 40
+// 45
+// 50
+
+// === Code Execution Successful ===

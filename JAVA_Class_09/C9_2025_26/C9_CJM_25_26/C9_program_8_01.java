fc3a063a8666216f8e8347a1c89314da048855b3
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-02-06 16:23:00 +0530

    Loop or Iterative

---
+++ JAVA_Class_09/C9_2025_26/C9_CJM_25_26/C9_program_8_01.java
@@ -0,0 +1,25 @@
+// Displaying Happy New Year
+
+class Main {
+
+  public static void main(String[] args) {
+    int a;
+    for (a = 1; a <= 10; a++) 
+        System.out.println("Happy New Year!!!");
+  }
+}
+
+// O/p
+
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+// Happy New Year!!!
+
+// === Code Execution Successful ===
---
+++ JAVA_Class_09/C9_2025_26/C9_CJM_25_26/C9_program_8_02.java
@@ -0,0 +1,27 @@
+// Displaying Natural Numbers
+
+class Main {
+
+  public static void main(String[] args) {
+    int a;
+    System.out.println("Natural numbers from 1 to 10:");
+    for (a = 1; a <= 10; a++) 
+        System.out.println(a);
+  }
+}
+
+// o/p
+
+// Natural numbers from 1 to 10:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// === Code Execution Successful ===

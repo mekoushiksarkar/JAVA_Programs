2fcef855a1f5a5b19cc4447ac4442a708eb8cf85
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-05-28 15:19:56 +0530

    For Krishnanagar Academy Students

---
+++ JAVA_Class_09/C9_2025_26/C9_KA_25_26/Numbers_While.java
@@ -0,0 +1,14 @@
+// Natural_Numbers_Using_WhileLoop.java
+
+public class Numbers_While 
+{
+  public static void main(String args[]) 
+  {
+    int i = 1;
+    while (i <= 5) 
+    {
+      System.out.println(i);
+      i++;
+    }
+  }
+}

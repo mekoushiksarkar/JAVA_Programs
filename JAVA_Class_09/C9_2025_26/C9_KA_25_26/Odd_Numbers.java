73dede5a3e2a464803cc47a78b01ab9963903551
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-06-03 12:33:41 +0530

    For Krishnanagar Academy Students

---
+++ JAVA_Class_09/C9_2025_26/C9_KA_25_26/Odd_Numbers.java
@@ -0,0 +1,13 @@
+/* OddNumbers.java */
+
+public class Odd_Numbers
+{
+    public static void main(String args[]) {
+        int oddNum = 11;
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(oddNum);
+            oddNum+=2;
+        }
+    }
+}

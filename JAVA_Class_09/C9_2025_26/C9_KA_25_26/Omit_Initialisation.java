e1eedb2b7435961e6bf03c8d218f9e346af28f26
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-06-11 12:34:11 +0530

    For Krishnanagar Academy Students

---
+++ JAVA_Class_09/C9_2025_26/C9_KA_25_26/Omit_Initialisation.java
@@ -0,0 +1,12 @@
+/* Omit_Initialisation.java */
+
+public class Omit_Initialisation
+{
+    public static void main(String args[]) {
+        int i = 5;
+        for (; i<= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}

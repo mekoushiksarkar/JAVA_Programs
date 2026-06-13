f9765b4834a8b1f7f0f8a16ecf8807962a754b8b
Koushik Sarkar <koushik.sarkar535@outlook.com>
2026-06-13 19:33:14 +0530

    For Krishnanagar Academy Students

---
+++ JAVA_Class_09/C9_2025_26/C9_KA_25_26/Omit_Update.java
@@ -0,0 +1,12 @@
+/* Omit_Update.java */
+
+public class Omit_Update
+{
+    public static void main(String args[]) {
+        for (int i = 7; i <= 10;)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
